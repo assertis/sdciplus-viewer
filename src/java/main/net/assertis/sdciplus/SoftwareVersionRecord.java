96671abd42031b6d035095b2bf9d6425a5ae9198
Daniel Dyer <dan@rectangularsoftware.com>
2009-11-12 16:16:34 +0000

    Fix for version record date. Fixed test configuration.

--- src/java/main/net/assertis/sdciplus/SoftwareVersionRecord.java
+++ src/java/main/net/assertis/sdciplus/SoftwareVersionRecord.java
@@ -1,12 +1,17 @@
 package net.assertis.sdciplus;
 
 import java.text.ParseException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  * @author Daniel Dyer
  */
 public class SoftwareVersionRecord extends SDCIPlusRecord
 {
+    // Reverse date format is used for software version records.
+    private static final DateFormat VERSION_RECORD_DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
+
     public SoftwareVersionRecord(String rawRecord) throws ParseException
     {
         String major = rawRecord.substring(0, 2);
@@ -14,7 +19,7 @@ public class SoftwareVersionRecord extends SDCIPlusRecord
         String micro = rawRecord.substring(4, 6);
         String version = major + '.' + minor + '.' + micro;
         addField("Version", version);
-        addField("Installation Date", DATE_TIME_FORMAT.parse(rawRecord.substring(6, 18)));
+        addField("Installation Date", VERSION_RECORD_DATE_FORMAT.parse(rawRecord.substring(6, 18)));
     }
 
 
